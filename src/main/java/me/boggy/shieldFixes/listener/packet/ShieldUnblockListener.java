da1397b15decb2fe42b011a04c254dae337cc2de
onurr_k <145716470+onurrrk@users.noreply.github.com>
2025-07-14 19:42:46 +0300

    Update ShieldUnblockListener.java

--- src/main/java/me/boggy/shieldFixes/listener/packet/ShieldUnblockListener.java
+++ src/main/java/me/boggy/shieldFixes/listener/packet/ShieldUnblockListener.java
@@ -1,12 +1,13 @@
 package me.boggy.shieldFixes.listener.packet;
 
 import com.github.retrooper.packetevents.event.PacketSendEvent;
+import com.github.retrooper.packetevents.event.PacketListener;
 import com.github.retrooper.packetevents.protocol.entity.data.EntityData;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityMetadata;
 import me.boggy.shieldFixes.ShieldFixes;
 
-public class ShieldUnblockListener implements com.github.retrooper.packetevents.event.PacketListener {
+public class ShieldUnblockListener implements PacketListener {
 
     private final ShieldFixes plugin;
 
@@ -14,14 +15,8 @@ public class ShieldUnblockListener implements com.github.retrooper.packetevents.
         this.plugin = plugin;
     }
 
-    /*
-       Detects when a player unblocks their shield so that if
-       the player blocks and unblocks within the 2 tick window
-       their shield doesn't stay visually blocking
-    */
     @Override
     public void onPacketSend(PacketSendEvent e) {
-
         if (e.getPacketType() != PacketType.Play.Server.ENTITY_METADATA) {
             return;
         }
@@ -30,12 +25,9 @@ public class ShieldUnblockListener implements com.github.retrooper.packetevents.
 
         for (EntityData entityData : packet.getEntityMetadata()) {
             if (entityData.getIndex() == 8 && entityData.getValue() instanceof Byte && (Byte) entityData.getValue() == 0) {
-                synchronized (plugin.getBlockingPlayers()) {
-                    plugin.getBlockingPlayers().removeIf(id -> id == packet.getEntityId());
-                }
+                plugin.getBlockingPlayers().removeIf(id -> id == packet.getEntityId());
                 return;
             }
         }
     }
-
 }
